9b8688420f7e76fc0dc83d13153aaae7402cec3c
Teguhriyadi <ilham.teguh55@gmail.com>
2023-11-13 09:05:26 +0700

    Update

--- resources/views/admin/layouts/partials/sidebar/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar/sidebar.blade.php
@@ -4,106 +4,49 @@
         data-kt-scroll-wrappers="#kt_app_sidebar_navs" data-kt-scroll-offset="5px">
         <div id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false"
             class="app-sidebar-menu-primary menu menu-column menu-rounded menu-sub-indention menu-state-bullet-primary">
-            <!-- home -->
-            <div class="menu-item mb-2">
-                <a href="./index.html" class="menu-link">
-                    <span class="menu-icon">
-                        <i class="fa-duotone fa-home fs-2"></i>
+
+            <?php foreach ($modul as $mod) : ?>
+                <?php if (count($mod["submodul"]) == 0) : ?>
+                <div class="menu-item mb-2">
+                    <a href="<?= route($mod["url"]) ?>" class="menu-link <?= jecho($modul_ini, $mod["slug"], "active") ?>">
+                        <span class="menu-icon">
+                            <i class="fa-duotone <?= $mod["ikon"] ?> fs-2"></i>
+                        </span>
+                        <span class="menu-title"><?= $mod["modul"] ?></span>
+                    </a>
+                </div>
+                <?php else : ?>
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion <?= jecho($modul_ini, $mod["slug"], "here show") ?>">
+                    <span class="menu-link <?= jecho($modul_ini, $mod['slug'], 'active') ?>">
+                        <span class="menu-icon">
+                            <i class="fa-duotone <?= $mod["ikon"] ?> fs-2"></i>
+                        </span>
+                        <span class="menu-title">
+                            <?= $mod["modul"] ?>
+                        </span>
+                        <span class="menu-arrow"></span>
                     </span>
-                    <span class="menu-title">Home</span>
-                </a>
-            </div>
-            <!-- info desa -->
-            <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-                <span class="menu-link">
-                    <span class="menu-icon">
-                        <i class="fa-duotone fa-info-circle fs-2"></i>
-                    </span>
-                    <span class="menu-title">
-                        Info Desa
-                    </span>
-                    <span class="menu-arrow"></span>
-                </span>
-                <div class="menu-sub menu-sub-accordion">
-                    <!-- identitas desa -->
-                    <div class="menu-item">
-                        <a class="menu-link" href="./identitas_desa.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">
-                                Identitas Desa
-                            </span>
-                        </a>
-                    </div>
-                    <!-- wilayah administratif -->
-                    <div class="menu-item">
-                        <a class="menu-link" href="./wilayah_administratif.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">
-                                Wilayah Administratif
-                            </span>
-                        </a>
-                    </div>
-                    <!-- pemerintah desa -->
-                    <div class="menu-item">
-                        <a class="menu-link" href="./buku_pemerintah_desa.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">
-                                Pemerintah Desa
-                            </span>
-                        </a>
-                    </div>
-                    <!-- status desa -->
-                    <div class="menu-item">
-                        <a class="menu-link" href="./status_desa.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">
-                                Status Desa
-                            </span>
-                        </a>
-                    </div>
-                    <!-- lembaga desa -->
-                    <div class="menu-item">
-                        <a class="menu-link" href="./lembaga_desa.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">
-                                Lembaga Desa
-                            </span>
-                        </a>
-                    </div>
-                    <!-- layanan pelanggan -->
-                    <div class="menu-item">
-                        <a class="menu-link" href="./layanan_pelanggan.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">
-                                Layanan Pelanggan
-                            </span>
-                        </a>
-                    </div>
-                    <!-- pendaftaran kerjasama -->
-                    <div class="menu-item">
-                        <a class="menu-link" href="./pendaftaran_kerjasama.html">
-                            <span class="menu-bullet">
-                                <span class="bullet bullet-dot"></span>
-                            </span>
-                            <span class="menu-title">
-                                Pendaftaran Kerjasama
-                            </span>
-                        </a>
+                    <div class="menu-sub menu-sub-accordion">
+                        <?php foreach ($mod["submodul"] as $submod) : ?>
+                        <div class="menu-item">
+                            <a class="menu-link <?= jecho($sub_modul_ini, $submod['slug'], 'active') ?>" href="<?= route($submod["url"]) ?>">
+                                <span class="menu-bullet">
+                                    <?php if ($submod["ikon"] != null) : ?>
+                                        <i class="fa-duotone <?= $submod["ikon"] ?>"></i>
+                                    <?php else : ?>
+                                        <span class="bullet bullet-dot"></span>
+                                    <?php endif ?>
+                                </span>
+                                <span class="menu-title">
+                                    <?= $submod["modul"] ?>
+                                </span>
+                            </a>
+                        </div>
+                        <?php endforeach ?>
                     </div>
                 </div>
-            </div>
+                <?php endif ?>
+            <?php endforeach ?>
         </div>
     </div>
 </div>
