ec070ba8fbfd14e874cc0c55441ec417dd82664e
Teguhriyadi <ilham.teguh55@gmail.com>
2023-11-15 11:10:14 +0700

    Updae

--- resources/views/admin/identitas_desa/index.blade.php
+++ resources/views/admin/identitas_desa/index.blade.php
@@ -37,18 +37,8 @@
 @endsection
 
 @section('content')
-    <div class="notice d-flex bg-light-warning rounded border-warning border border-dashed p-6 mb-10">
-        <i class="fa-duotone fa-triangle-exclamation fs-2qx text-warning me-4"></i>
-        <div class="d-flex flex-stack flex-grow-1">
-            <div class=" fw-semibold">
-                <h4 class="text-gray-900 fw-bold">Informasi</h4>
-                <div class="fs-6 text-gray-700 ">
-                    Silahkan tentukan Kepala Desa {{ $main->nama_desa }}
-                    melalui modul <a class="fw-bold" href="./buku_peraturan_desa.html">Pemerintah Desa</a>.
-                </div>
-            </div>
-        </div>
-    </div>
+    
+    @include("admin.identitas_desa.info_kades")
 
     <div class="d-flex flex-column flex-lg-row gap-5 gap-xl-8">
         <div class="d-flex flex-column flex-lg-row-auto w-lg-250px w-xl-350px">
--- resources/views/admin/identitas_desa/info_kades.blade.php
+++ resources/views/admin/identitas_desa/info_kades.blade.php
@@ -1,11 +1,13 @@
 @if (!$cek_kades)
-    <div class="row">
-        <div class="col-md-12">
-            <div class="callout callout-warning">
-                <h4><i class="fa fa-warning"></i>&nbsp;&nbsp;Informasi</h4>
-                <p>Silahkan tentukan {{ ucwords(setting('sebutan_kepala_desa') . ' ' . $main->nama_desa) }} melalui
-                    modul <a href="{{ route('pengurus') }}"><b>{{ ucwords(setting('sebutan_pemerintah_desa')) }}</b></a>
-                </p>
+    <div class="notice d-flex bg-light-warning rounded border-warning border border-dashed p-6 mb-10">
+        <i class="fa-duotone fa-triangle-exclamation fs-2qx text-warning me-4"></i>
+        <div class="d-flex flex-stack flex-grow-1">
+            <div class=" fw-semibold">
+                <h4 class="text-gray-900 fw-bold">Informasi</h4>
+                <div class="fs-6 text-gray-700 ">
+                    Silahkan tentukan Kepala Desa {{ $main->nama_desa }}
+                    melalui modul <a class="fw-bold" href="<?= site_url('pengurus') ?>">Pemerintah Desa</a>.
+                </div>
             </div>
         </div>
     </div>
